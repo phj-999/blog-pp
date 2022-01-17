e4cfc9f8b6922fa53e130a07a1ee01ce2cc1d47f
phj-999 <865779043@qq.com>
2022-01-17 16:37:42 +0800

    加入jsonserver后遍历动态渲染的侧边栏

--- backend-xw/src/components/sandbox/SideMenu.js
+++ backend-xw/src/components/sandbox/SideMenu.js
@@ -1,72 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Layout, Menu } from "antd";
-import { withRouter } from "react-router-dom";
 import { UserOutlined } from "@ant-design/icons";
 import { useHistory } from "react-router";
+import axios from "axios";
 import "./index.css";
-//模拟数组结构
-const menuList = [
-  {
-    key: "/home",
-    title: "首页",
-    icon: <UserOutlined />,
-  },
-  {
-    key: "/user-manage",
-    title: "用户管理",
-    icon: <UserOutlined />,
-    children: [
-      {
-        key: "/user-manage/list",
-        title: "用户列表",
-        icon: <UserOutlined />,
-      },
-    ],
-  },
-  {
-    key: "/right-manage",
-    title: "权限管理",
-    icon: <UserOutlined />,
-    children: [
-      {
-        key: "/right-manage/role/list",
-        title: "角色列表",
-        icon: <UserOutlined />,
-      },
-      {
-        key: "/right-manage/right/list",
-        title: "权限列表",
-        icon: <UserOutlined />,
-      },
-    ],
-  },
-];
+
+const iconList = {
+  "/home": <UserOutlined />,
+  "/user-manage": <UserOutlined />,
+  "/user-manage/list": <UserOutlined />,
+  "/right-manage": <UserOutlined />,
+  "/right-manage/role/list": <UserOutlined />,
+  "/right-manage/right/list": <UserOutlined />,
+  //.......
+};
 
 const SideMenu = () => {
   const { Sider } = Layout;
   const { SubMenu } = Menu;
   const history = useHistory();
+  const [meun, setMeun] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/rights?_embed=children")
+      .then((response) => {
+        console.log(response);
+        setMeun(response.data);
+      });
+  }, []);
+
+  const checkPagePermission = (item) => {
+    return item.pagepermisson;
+  };
+
   const renderMenu = (menuList) => {
     return menuList.map((item) => {
-      if (item.children) {
+      if (item.children && checkPagePermission(item)) {
         return (
-          <SubMenu kry={item.key} icon={item.icon} title={item.title}>
+          <SubMenu key={item.key} icon={iconList[item.key]} title={item.title}>
             {renderMenu(item.children)}
           </SubMenu>
         );
       }
 
       return (
-        <Menu.Item
-          key={item.key}
-          icon={item.icon}
-          onClick={() => {
-            //  console.log(props)
-            history.push(item.key);
-          }}
-        >
-          {item.title}
-        </Menu.Item>
+        checkPagePermission(item) && (
+          <Menu.Item
+            key={item.key}
+            icon={iconList[item.key]}
+            onClick={() => {
+              //  console.log(props)
+              history.push(item.key);
+            }}
+          >
+            {item.title}
+          </Menu.Item>
+        )
       );
     });
   };
@@ -75,9 +65,9 @@ const SideMenu = () => {
     <Sider trigger={null} collapsible collapsed={false}>
       <div className="logo">全球新闻发布管理系统</div>
       <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-        {renderMenu(menuList)}
+        {renderMenu(meun)}
       </Menu>
     </Sider>
   );
 };
-export default SideMenu
+export default SideMenu;
