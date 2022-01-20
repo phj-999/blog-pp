69d1a125793d29beca041037de99efbe3e46eacd
phj-999 <865779043@qq.com>
2022-01-20 22:01:08 +0800

    角色列表+删除功能

--- backend-xw/src/views/sandbox/right-manage/RoleList.jsx
+++ backend-xw/src/views/sandbox/right-manage/RoleList.jsx
@@ -1,11 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Table, Button, Modal } from "antd";
+import {
+  DeleteOutlined,
+  EditOutlined,
+  ExclamationCircleOutlined,
+} from "@ant-design/icons";
+import axios from "axios";
 
 const RoleList = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [dataSource, setdataSource] = useState([]);
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      render: (id) => {
+        return <b>{id}</b>;
+      },
+    },
+    {
+      title: "角色名称",
+      dataIndex: "roleName",
+    },
+    {
+      title: "操作",
+      render: (item) => {
+        return (
+          <div>
+            <Button
+              danger
+              shape="circle"
+              icon={<DeleteOutlined />}
+              onClick={() => confirmMethod(item)}
+            />
+            <Button type="primary" shape="circle" icon={<EditOutlined />} />
+          </div>
+        );
+      },
+    },
+  ];
 
-export default RoleList
+  const confirmMethod = (item) => {
+    const { confirm } = Modal;
+    confirm({
+      title: "你确定要删除?",
+      icon: <ExclamationCircleOutlined />,
+      onOk() {
+        //   console.log('OK');
+        deleteMethod(item);
+      },
+      onCancel() {
+        //   console.log('Cancel');
+      },
+    });
+  };
+  //删除
+  const deleteMethod = (item) => {
+    // console.log(item)
+    setdataSource(dataSource.filter((data) => data.id !== item.id));
+    axios.delete(`http://localhost:3000/roles/${item.id}`);
+  };
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/roles").then((res) => {
+      // console.log(res.data)
+      setdataSource(res.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Table
+        dataSource={dataSource}
+        columns={columns}
+        rowKey={(item) => item.id}
+      ></Table>
+    </div>
+  );
+};
+
+export default RoleList;
