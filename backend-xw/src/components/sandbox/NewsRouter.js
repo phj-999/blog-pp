ab86ecfa4a257b2029cf7091fcc4be3a1ed9f897
phj-999 <865779043@qq.com>
2022-01-23 16:50:57 +0800

    NewsRouter类里面根据返回接口结合映射表遍历动态创建路由

--- backend-xw/src/components/sandbox/NewsRouter.js
+++ backend-xw/src/components/sandbox/NewsRouter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 import Home from "../../views/sandbox/home/Home";
@@ -17,29 +17,53 @@ import AuditList from "../../views/sandbox/audit-manage/Audit";
 import Unpublished from "../../views/sandbox/publish-manage/Unpublished";
 import Published from "../../views/sandbox/publish-manage/Published";
 import Sunset from "../../views/sandbox/publish-manage/Sunset";
+import axios from "axios";
 
 const LocalRouterMap = {
   "/home": Home,
-  "user-mange/list": UserList,
-  "/right-mange/role/list": RoleList,
-  "/right-mange/right/list": RightList,
+  "user-manage/list": UserList,
+  "/right-manage/role/list": RoleList,
+  "/right-manage/right/list": RightList,
   "/news-manage/add": NewsAdd,
   "/news-manage/draft": NewDraft, //草稿箱,
   "/news-manage/category": NewsCategory,
-  "/audit/manage/audit": Audit,
-  "/audit/manage/list": AuditList,
+  "/audit-manage/audit": Audit,
+  "/audit-manage/list": AuditList,
   "/publish-manage/unpublished": Unpublished, //待发布
   "/publish-manage/published": Published, // 已发布
   "/publish-manage/sunset": Sunset, // 已上线
 };
 
 const NewsRouter = () => {
+  const [backRouteList, setBackRouteList] = useState([]);
+  
+  useEffect(()=>{
+    Promise.all([
+        axios.get("http://localhost:3000/rights"),
+        axios.get("http://localhost:3000/children"),
+    ]).then(res=>{
+        // console.log(res)
+        setBackRouteList([...res[0].data,...res[1].data])
+         console.log(backRouteList)
+    })
+},[])
+
   return (
     <Switch>
-      <Route path="/home" component={Home} exact />
+        {/* 遍历路由 */}
+        {
+            backRouteList.map(item=>
+               {
+              
+                       return  <Route exact path={item.key} key={item.key} component={LocalRouterMap[item.key]}/>
+              
+               }
+            )
+        }
+      {/* <Route path="/home" component={Home} exact />
       <Route path="/user-manage/list" component={UserList} />
       <Route path="/right-manage/role/list" component={RoleList} />
-      <Route path="/right-manage/right/list" component={RightList} />
+      <Route path="/right-manage/right/list" component={RightList} /> */}
 
       <Redirect from="/" to="/home" exact />
       <Route path="*" component={Nopermission} />
