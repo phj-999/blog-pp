047c8c62cc5f51cdc6892e9b96c67d43d8258d6b
phj-999 <865779043@qq.com>
2022-01-23 16:20:33 +0800

    路由权限-本地映射表

--- backend-xw/src/components/sandbox/NewsRouter.js
+++ backend-xw/src/components/sandbox/NewsRouter.js
@@ -7,6 +7,32 @@ import RightList from "../../views/sandbox/right-manage/RightList";
 import RoleList from "../../views/sandbox/right-manage/RoleList";
 import UserList from "../../views/sandbox/user-manage/UserList";
 
+import NewsAdd from "../../views/sandbox/news-manage/NewsAdd";
+import NewDraft from "../../views/sandbox/news-manage/NewDraft";
+import NewsCategory from "../../views/sandbox/news-manage/NewsCategory";
+
+import Audit from "../../views/sandbox/audit-manage/Audit";
+import AuditList from "../../views/sandbox/audit-manage/Audit";
+
+import Unpublished from "../../views/sandbox/publish-manage/Unpublished";
+import Published from "../../views/sandbox/publish-manage/Published";
+import Sunset from "../../views/sandbox/publish-manage/Sunset";
+
+const LocalRouterMap = {
+  "/home": Home,
+  "user-mange/list": UserList,
+  "/right-mange/role/list": RoleList,
+  "/right-mange/right/list": RightList,
+  "/news-manage/add": NewsAdd,
+  "/news-manage/draft": NewDraft, //草稿箱,
+  "/news-manage/category": NewsCategory,
+  "/audit/manage/audit": Audit,
+  "/audit/manage/list": AuditList,
+  "/publish-manage/unpublished": Unpublished, //待发布
+  "/publish-manage/published": Published, // 已发布
+  "/publish-manage/sunset": Sunset, // 已上线
+};
+
 const NewsRouter = () => {
   return (
     <Switch>
