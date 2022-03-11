939e938332d5e8bc1f0cdacb152d8ee64d3bc674
phj-999 <865779043@qq.com>
2022-03-11 23:53:52 +0800

    自适应

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-//
+//自适应  修改设置成24等份，设计稿时1920px的，这样1rem就是80px （1920/24=80）方便使用
+import 'lib-flexible/flexible.js'
 
 createApp(App).use(store).use(router).mount('#app')
