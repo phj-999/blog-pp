1f049bd9b7ab3cf6142d99011502f3a10062faa7
phj-999 <865779043@qq.com>
2022-03-20 22:45:29 +0800

    echarts接口修改

--- routes/echartsApi/index.js
+++ routes/echartsApi/index.js
@@ -1,10 +1,10 @@
 let express=require("express");
 let router=express.Router()
-let mapData=require("../../public/mock/china.json")
-let oneData=require("../../public/mock/one.json")
-let twoData=require("../../public/mock/two.json")
-let threeData=require("../../public/mock/three.json")
-let fourData=require("../../public/mock/four.json")
+let mapData=require("../../public/mock/echarts/china.json")
+let oneData=require("../../public/mock/echarts/one.json")
+let twoData=require("../../public/mock/echarts/two.json")
+let threeData=require("../../public/mock/echarts/three.json")
+let fourData=require("../../public/mock/echarts/four.json")
 
 router.get("/mapdata",(req,res)=>{
     res.send({msg:"我是地图的路由地址",chinaData:mapData})
